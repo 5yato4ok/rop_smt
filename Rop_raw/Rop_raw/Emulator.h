130135cc2f371a20d5b6c52e1bde90069bee889b
5yato4ok <pyatochok37@gmail.com>
2017-10-19 09:00:01 +0300

    Revert to working commit

---
+++ Rop_raw/Rop_raw/Emulator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "unicorn\unicorn.h"
+class Emulator {
+  Emulator() {};
+  uc_engine *uc;
+  //arch);
+  //arch = arch;//structure with architectur and mode
+  //uc = unicorn.uc(ar);
+  ////get_item?
+  ////setitem?
+  //map_addres(adress, length);
+  //map_code(adress, code);
+  //setup_stack(adress, size, data = none);
+  //run(adress, size);
+};
